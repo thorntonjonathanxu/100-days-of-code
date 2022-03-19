67d15b3f1f8027944f90a3ae0318c01ea9d9a3ba
Jon Thornton <thorntonjonathanxu@gmail.com>
2022-03-19 16:21:29 -0400

    Wordle Description Content

--- Wordle/wordle-solver.py
+++ Wordle/wordle-solver.py
@@ -4,18 +4,19 @@ import enchant
 import webbrowser
 import pyautogui
 import time
+from bs4 import BeautifulSoup
+from string import ascii_lowercase
 
 # Function activates GoogleChrome application and opens a new Tab.
 def openBrowser():
     url = 'https://www.nytimes.com/games/wordle/index.html'
     chrome_path = r'C:/Program Files/Google/Chrome/Application/chrome.exe %s --incognito'
     webbrowser.get(chrome_path).open_new(url)
-    pyautogui.press('left')
     
-    # Click screen to remove pop-up that appears
-    
+# Function closes the current GoogleChrome window.
+def closeBrowser():    
     # Close Web Browser
-    time.sleep(20)
+    time.sleep(3)
     pyautogui.hotkey('ctrl', 'w')
     print("tab closed")
 
@@ -23,17 +24,36 @@ def openBrowser():
 
 # Function goes through the gameboard and loads the alphabet into a list of characters that 
 # are currently active with the applicable status. 
-    # Status 0 - Acitve but Not Used
-    # Status 1 - Active and Wrong Space
-    # Status 2 - Active and Correct
-    # Status 3 - Inactive
+    # Status 0 -  - letter has not be used in a word (default state)
+    # Status 1 - present - letter has been used but incorrect location
+    # Status 2 - correct - letter has been used and correct location
+    # Status 3 - absent - letter used but not used
+
 def alphabetlist():
+    # Assume location on screen is always consistent
+    # BeautifulSoup read in the html content from the current browser
+    
+    #Sample: <game-tile letter="w" evaluation="present" reveal=""></game-tile>
+    # To obtian status we need to check the [evaluation] attribute for each [game-title] element
+    
+    # Grab All instances of "game-tile"
+    # Loop through each letter until attribute doesn't exist then break
+        # If found update dictionary
 
     return False
+
+# Function generates a dictionary with all lowercase alphabet letters as the key and status as the value.
+def builtalphabetdict():
+    dict = {}
+    for c in ascii_lowercase:
+        dict[c] = 0
+    return dict
 
 def main():
-    openBrowser()
-    return False
+    # openBrowser()
+    letters = builtalphabetdict()
+    print(letters)
+    alphabetlist()
 
 if __name__ == "__main__":
     main()
