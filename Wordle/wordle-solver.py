02843c724739a9bd7e3e418d4dfa88df9903c042
Jon Thornton <thorntonjonathanxu@gmail.com>
2022-03-20 19:39:18 -0400

    Basic word check complete

--- Wordle/wordle-solver.py
+++ Wordle/wordle-solver.py
@@ -56,6 +56,7 @@ def getpossiblewords() -> list:
     words = []
     with open('.\Wordle\words.csv','r') as f:
         for row in f:
+            row = row.strip('\n')[0:]
             words.append(row)
     return words
 
@@ -64,14 +65,12 @@ def getgameword(words:list) -> str:
     random.shuffle(words)
     return words[0]
 
+# Function takes in user input, checks that it is a valid word, and returns the word. 
 def guessword(words:list) -> str:
-    # while True:
-        # try:
-    guess = input('Please choose a word')
-        # except ValueError:
-        #     print("Not a! Please enter an integer.")
-        #     continue
-        # if len(guess) != 5 and guess not in words:
+    guess = ''
+    while guess not in words:
+        guess = input('Please choose a word:\n')
+        print(guess)
     return guess
 
 def printboard(guesses:list, alphabetlist:list) -> None:
@@ -81,6 +80,9 @@ def printboard(guesses:list, alphabetlist:list) -> None:
         else:
             print(f'{i} ')
     print(alphabetlist)
+
+# def checkword(guess:str,solution:str, alphabetlist:dict):
+    # for i in list(guess):
 
 
 def main():
@@ -99,10 +101,51 @@ def main():
     letters = builtalphabetdict()
     alphabetlist = letters.keys()
     printboard(guesses, alphabetlist)
+    print(gameword)
+    n = 0 
+    ## GAME ROUNDS 
+    while n < 2:
+        # User Input is taken in
+        guesses.append(guessword(words))
 
-   ## GAME ROUNDS 
-   # User Input is taken in
-   # Append Users input 
+        # Find the bisection of the guess word and the solution word
+        # correct_letters = list(set(list(guesses[0])).intersection(set(list(gameword))))
+
+        
+        
+        # If correct letters is not null, loop through solution and guess word 
+        temp1 = []
+        temp2 = []
+        for idx, i in enumerate(list(guesses[len(guesses)-1])):
+            temp1.append([idx,i])
+        for idx, i in enumerate(list(gameword)):
+            temp2.append([idx,i])
+
+        for i in range(5):
+            for j in range(5):
+                if temp1[i] == temp2[i]:
+                    letters[temp1[i][1]] = 2
+                    print('Correct', temp1[i], temp2[i])        
+                elif temp1[i][1] == temp2[j][1] and i != j:
+                    letters[temp1[i][1]] = 1
+                    print('Present', temp1[i], temp2[j])
+                else:
+                    continue                
+
+
+            # if i in list(gameword) and idx == idy:
+            #     letters[i] = 2
+            #     print(i, idx, f'Correct - {idx}')
+            # elif i in list(gameword):
+            #     letters[i] = 1
+            #     print(i, idx, f'Present not in {idx}')
+            # else:
+            #     # print(i, 'Not Present')
+            #     continue
+        for key, val in letters.items():
+            if val != 0:
+                print(key,val)
+        n += 1
 
 if __name__ == "__main__":
     main()
